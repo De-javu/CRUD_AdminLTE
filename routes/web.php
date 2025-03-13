0573620d24646c272434cf7d20df2a23d9077f68
Andres <andrespardo5151@gmail.com>
2025-03-12 19:37:25 -0500

    CRUD CON  LARAVEL ADMINLTE

---
+++ routes/web.php
@@ -0,0 +1,41 @@
+<?php
+
+use App\Http\Controllers\AdminController;
+use App\Livewire\Settings\Appearance;
+use App\Http\Controllers\UsuarioController;
+use App\Livewire\Settings\Password;
+use App\Livewire\Settings\Profile;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
+Route::get('/', function () {
+    return view('welcome');
+})->name('home');
+
+
+
+
+Route::get('/usuarios', [UsuarioController::class, 'index'])->name('usuarios');
+
+
+
+
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+Route::middleware(['auth'])->group(function () {
+    Route::redirect('settings', 'settings/profile');
+
+    Route::get('settings/profile', Profile::class)->name('settings.profile');
+    Route::get('settings/password', Password::class)->name('settings.password');
+    Route::get('settings/appearance', Appearance::class)->name('settings.appearance');
+
+    Route::get('/admin', [AdminController::class, 'index'])->name('admin');
+});
+
+require __DIR__.'/auth.php';
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
